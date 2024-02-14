2bac18d8a54b2fd25ba8b0264a74f94f4ca96e3d
Tom Vieira <skixoxgaming@gmail.com>
2024-02-14 14:09:50 +0100

    Implémentation du MainActivity et du layout_mainactivity.xml

--- androidApp/src/main/java/fr/iut/mytodolist/android/MainActivity.kt
+++ androidApp/src/main/java/fr/iut/mytodolist/android/MainActivity.kt
@@ -1,40 +1,45 @@
-package fr.iut.mytodolist.android
-
+package com.example.todolist
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import fr.iut.mytodolist.Greeting
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetingView(Greeting().greet())
-                }
+class MainActivity : AppCompatActivity() {
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        var selectedFragment: Fragment? = null
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                selectedFragment = HomeFragment()
+            }
+            R.id.navigation_settings -> {
+                selectedFragment = SettingsFragment()
+            }
+            R.id.navigation_notifications -> {
+                selectedFragment = NotificationFragment()
             }
         }
+
+        if (selectedFragment != null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, selectedFragment)
+            transaction.commit()
+        }
+
+        true
     }
-}
 
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_mainactivity)
 
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.commit()
+
+        navView.selectedItemId = R.id.navigation_home
     }
 }
