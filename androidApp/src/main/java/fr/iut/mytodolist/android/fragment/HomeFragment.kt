aed6795c7506767c820f79274a7e945f90e54a3d
Tom Vieira <skixoxgaming@gmail.com>
2024-02-16 15:58:02 +0100

    Ajout d'un PageView avec la création de classe

--- androidApp/src/main/java/fr/iut/mytodolist/android/fragment/HomeFragment.kt
+++ androidApp/src/main/java/fr/iut/mytodolist/android/fragment/HomeFragment.kt
@@ -1,22 +1,29 @@
 package fr.iut.mytodolist.android.fragment
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
 import fr.iut.mytodolist.android.R
+import fr.iut.mytodolist.android.TodoPagerAdapter
 
 class HomeFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView = view.findViewById<TextView>(R.id.text)
-        textView.text = "Home"
-        return view
-    }
+override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+): View? {
+    val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+    // Initialisez le ViewPager et définissez l'adaptateur
+    val viewPager = view.findViewById<ViewPager>(R.id.viewPager)
+    val pagerAdapter = TodoPagerAdapter(childFragmentManager)
+    viewPager.adapter = pagerAdapter
+
+    return view
+}
 }
