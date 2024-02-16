df975af1c219235ab87fcaeb15a16eb4f6dcda38
Tom Vieira <skixoxgaming@gmail.com>
2024-02-16 16:12:57 +0100

    Ajout d'une naviguation bar pour savoir sur quoi on est

--- androidApp/src/main/java/fr/iut/mytodolist/android/fragment/HomeFragment.kt
+++ androidApp/src/main/java/fr/iut/mytodolist/android/fragment/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import fr.iut.mytodolist.android.R
 import fr.iut.mytodolist.android.TodoPagerAdapter
 
@@ -19,11 +20,17 @@ override fun onCreateView(
 ): View? {
     val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+    // Initialisez le ViewPager et définissez l'adaptateur
     val viewPager = view.findViewById<ViewPager>(R.id.viewPager)
     val pagerAdapter = TodoPagerAdapter(childFragmentManager)
     viewPager.adapter = pagerAdapter
 
+    // Définir la page par défaut
     viewPager.setCurrentItem(1)
+
+    // Liez le TabLayout au ViewPager
+    val tabLayout = view.findViewById<TabLayout>(R.id.tabLayout)
+    tabLayout.setupWithViewPager(viewPager)
 
     return view
 }
