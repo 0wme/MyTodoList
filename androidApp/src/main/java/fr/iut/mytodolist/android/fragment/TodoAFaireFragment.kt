c9c4d12afaa600d3d8cbf3dc7b1c6af92de8b6fc
Tom <skixoxgaming@gmail.com>
2024-02-17 17:29:22 +0100

    Ajout des ToDos quand on clique sur le bouton (enfin)

--- androidApp/src/main/java/fr/iut/mytodolist/android/fragment/TodoAFaireFragment.kt
+++ androidApp/src/main/java/fr/iut/mytodolist/android/fragment/TodoAFaireFragment.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import fr.iut.mytodolist.android.R
 
 class TodoAFaireFragment : Fragment() {
+
+    private val todoList = mutableListOf<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,16 +23,50 @@ class TodoAFaireFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_todo_a_faire, container, false)
 
+        val todoRecyclerView = view.findViewById<RecyclerView>(R.id.todoRecyclerView)
+        todoRecyclerView.layoutManager = LinearLayoutManager(context)
+        todoRecyclerView.adapter = TodoAdapter(todoList)
+
         val addTodoButton = view.findViewById<ImageButton>(R.id.addTodoButton)
         addTodoButton.setOnClickListener {
-            val fragmentManager = fragmentManager
-            fragmentManager?.let {
-                val fragmentTransaction = it.beginTransaction()
-                fragmentTransaction.addToBackStack(null)
-                fragmentTransaction.commit()
-            }
+            val todoEditText = EditText(it.context)
+            val dialog = AlertDialog.Builder(it.context)
+                .setTitle("Quel est votre To Do ?")
+                .setView(todoEditText)
+                .setPositiveButton("Ajouter") { dialog, _ ->
+                    val todo = todoEditText.text.toString()
+                    if (todo.isNotEmpty()) {
+                        todoList.add(todo)
+                        dialog.dismiss()
+                        (todoRecyclerView.adapter as TodoAdapter).notifyDataSetChanged()
+                    }
+                }
+                .setNegativeButton("Annuler") { dialog, _ ->
+                    dialog.cancel()
+                }
+                .create()
+            dialog.show()
         }
 
         return view
+    }
+
+    class TodoAdapter(private val todoList: List<String>) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+
+        class TodoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val todoTextView: TextView = view.findViewById(R.id.todoTextView)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.todo_item, parent, false)
+            return TodoViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+            val todo = todoList[position]
+            holder.todoTextView.text = todo
+        }
+
+        override fun getItemCount() = todoList.size
     }
 }
