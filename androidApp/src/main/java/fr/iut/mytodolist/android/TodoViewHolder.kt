d177f475a5baa47faa463175ab5760027dce9c6f
Tom <skixoxgaming@gmail.com>
2024-02-20 21:59:59 +0100

    Optimisation de code

--- androidApp/src/main/java/fr/iut/mytodolist/android/TodoViewHolder.kt
+++ androidApp/src/main/java/fr/iut/mytodolist/android/TodoViewHolder.kt
@@ -8,7 +8,6 @@ import androidx.recyclerview.widget.RecyclerView
 
 class TodoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     val todoTextView: TextView = view.findViewById(R.id.todoTextView)
-    val descriptionTextView: TextView = view.findViewById(R.id.descriptionTextView)
     val buttonLayout: LinearLayout = view.findViewById(R.id.buttonLayout)
     val approveButton: ImageButton = view.findViewById(R.id.approveButton)
     val cancelButton: ImageButton = view.findViewById(R.id.cancelButton)
