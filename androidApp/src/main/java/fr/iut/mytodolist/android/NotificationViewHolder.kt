fbdcd37b2e9299824c960eb2a1d59ab5f587beb6
Tom <skixoxgaming@gmail.com>
2024-02-27 18:47:12 +0100

    modificatino de NotificationViewHolder

--- androidApp/src/main/java/fr/iut/mytodolist/android/NotificationViewHolder.kt
+++ androidApp/src/main/java/fr/iut/mytodolist/android/NotificationViewHolder.kt
@@ -1,9 +1,11 @@
 package fr.iut.mytodolist.android
 
 import android.view.View
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class NotificationViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     val notificationTextView: TextView = view.findViewById(R.id.notificationTextView)
+    val cancelButton: ImageButton = view.findViewById(R.id.cancelButton)
 }
