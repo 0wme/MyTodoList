678dfa874903ce7edfc9e3ec560901232fc7bfe7
Tom <skixoxgaming@gmail.com>
2024-02-27 18:26:33 +0100

    Modification de l'esthétisme des notifications

--- androidApp/src/main/java/fr/iut/mytodolist/android/NotificationAdapter.kt
+++ androidApp/src/main/java/fr/iut/mytodolist/android/NotificationAdapter.kt
@@ -1,10 +1,17 @@
 package fr.iut.mytodolist.android
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class NotificationAdapter(private val notificationList: MutableList<String>) : RecyclerView.Adapter<NotificationViewHolder>() {
+class NotificationAdapter(private val notificationList: List<String>) :
+    RecyclerView.Adapter<NotificationAdapter.NotificationViewHolder>() {
+
+    class NotificationViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val notificationTextView: TextView = view.findViewById(R.id.notificationTextView)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotificationViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.notification_item, parent, false)
