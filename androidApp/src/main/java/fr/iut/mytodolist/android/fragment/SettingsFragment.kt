e8c501d1274d886726bd6461c5d3fe95696df00b
Tom <skixoxgaming@gmail.com>
2024-02-28 23:40:12 +0100

    Modification du code pour le btn param

--- androidApp/src/main/java/fr/iut/mytodolist/android/fragment/SettingsFragment.kt
+++ androidApp/src/main/java/fr/iut/mytodolist/android/fragment/SettingsFragment.kt
@@ -1,16 +1,19 @@
 package fr.iut.mytodolist.android.fragment
 
+import android.content.Intent
+import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import fr.iut.mytodolist.android.R
-import android.content.res.Configuration
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
+import androidx.fragment.app.Fragment
+import fr.iut.mytodolist.android.R
 import java.util.Locale
 
 class SettingsFragment : Fragment() {
@@ -43,6 +46,14 @@ class SettingsFragment : Fragment() {
             override fun onNothingSelected(parent: AdapterView<*>) {
                 // Ne rien faire
             }
+        }
+
+        val openSettingsButton = view.findViewById<Button>(R.id.open_settings_button)
+        openSettingsButton.setOnClickListener {
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            val uri = Uri.fromParts("package", requireActivity().packageName, null)
+            intent.data = uri
+            startActivity(intent)
         }
 
         return view
