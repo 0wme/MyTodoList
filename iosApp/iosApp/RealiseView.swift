932f1203323bf94e9dc592307ef575ff44fb125e
Tom Vieira <skixoxgaming@gmail.com>
2024-03-02 23:26:36 +0100

    Modification de RealiseView

--- iosApp/iosApp/RealiseView.swift
+++ iosApp/iosApp/RealiseView.swift
@@ -1,7 +1,32 @@
 import SwiftUI
 
 struct RealiseView: View {
+    @EnvironmentObject var todoManager: TodoManager
+
     var body: some View {
-        Text("Contenu pour Réalisé !")
+        List {
+            ForEach(todoManager.todosRealise) { todo in
+                HStack {
+                    Text(todo.title)
+                        .foregroundColor(.primary)
+                    Spacer()
+                    // Ajoutez ici des boutons ou des actions si vous souhaitez modifier le statut du todo ou le supprimer
+                }
+                .padding()
+                .cornerRadius(10)
+                .shadow(radius: 2)
+            }
+            .onDelete(perform: removeTodoFromRealise)
+        }
+        .navigationTitle("Réalisé")
+    }
+
+    private func removeTodoFromRealise(at offsets: IndexSet) {
+        withAnimation {
+            offsets.forEach { index in
+                let todo = todoManager.todosRealise[index]
+                todoManager.removeTodoFromRealise(todo)
+            }
+        }
     }
 }
