3ba66d9cf3ed90dd2c4920ac22348f867075f5c2
Tom Vieira <skixoxgaming@gmail.com>
2024-03-02 21:51:49 +0100

    Modification de AddTodoView pour laisser le choix de mettre une Date et/ou une heure

--- iosApp/iosApp/AddTodoView.swift
+++ iosApp/iosApp/AddTodoView.swift
@@ -3,22 +3,34 @@ import SwiftUI
 struct AddTodoView: View {
     @Environment(\.dismiss) var dismiss
     @State private var title: String = ""
+    @State private var addDate: Bool = false
+    @State private var addTime: Bool = false
     @State private var date: Date = Date()
     @State private var time: Date = Date()
-    var onComplete: (String, Date, Date) -> Void
+    var onComplete: (String, Date?, Date?) -> Void
     
     var body: some View {
         NavigationView {
             Form {
                 TextField("Quel todo on veut ajouter ?", text: $title)
-                DatePicker("Date", selection: $date, displayedComponents: .date)
-                DatePicker("Heure", selection: $time, displayedComponents: .hourAndMinute)
+                Toggle(isOn: $addDate) {
+                    Text("Ajouter une date")
+                }
+                if addDate {
+                    DatePicker("Date", selection: $date, displayedComponents: .date)
+                }
+                Toggle(isOn: $addTime) {
+                    Text("Ajouter une heure")
+                }
+                if addTime {
+                    DatePicker("Heure", selection: $time, displayedComponents: .hourAndMinute)
+                }
             }
             .navigationTitle("Ajouter un todo")
             .navigationBarItems(leading: Button("Annuler") {
                 dismiss()
             }, trailing: Button("Ajouter") {
-                onComplete(title, date, time)
+                onComplete(title, addDate ? date : nil, addTime ? time : nil)
                 dismiss()
             })
         }
