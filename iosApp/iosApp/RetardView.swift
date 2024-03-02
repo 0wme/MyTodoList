7512763be417beb624578fb1bcb1e91c4b1137b1
Tom Vieira <skixoxgaming@gmail.com>
2024-03-02 23:26:57 +0100

    Modification de RetardView

--- iosApp/iosApp/RetardView.swift
+++ iosApp/iosApp/RetardView.swift
@@ -1,7 +1,32 @@
 import SwiftUI
 
 struct RetardView: View {
+    @EnvironmentObject var todoManager: TodoManager
+
     var body: some View {
-        Text("Contenu pour Retard")
+        List {
+            ForEach(todoManager.todosRetard) { todo in
+                HStack {
+                    Text(todo.title)
+                        .foregroundColor(.primary)
+                    Spacer()
+                    // Ajoutez ici des boutons ou des actions si vous souhaitez modifier le statut du todo ou le supprimer
+                }
+                .padding()
+                .cornerRadius(10)
+                .shadow(radius: 2)
+            }
+            .onDelete(perform: removeTodoFromRetard)
+        }
+        .navigationTitle("Retard")
+    }
+
+    private func removeTodoFromRetard(at offsets: IndexSet) {
+        withAnimation {
+            offsets.forEach { index in
+                let todo = todoManager.todosRetard[index]
+                todoManager.removeTodoFromRetard(todo)
+            }
+        }
     }
 }
