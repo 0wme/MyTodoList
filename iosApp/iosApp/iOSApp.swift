9187316726c62aad95d4c8fd44bae5f990141cb5
Tom Vieira <skixoxgaming@gmail.com>
2024-03-05 16:45:01 +0100

    Modification de IOSApp pour pouvoir mettre à jour NotificationView

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,34 +1,39 @@
-import SwiftUI
-import UserNotifications
+    import SwiftUI
+    import UserNotifications
 
-@main
-struct iOSApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    
-    var todoManager = TodoManager()
+    @main
+    struct iOSApp: App {
+        @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environmentObject(todoManager)
-        }
-    }
-}
-
-class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        // Demande d'autorisation pour les notifications
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, _ in
-            if granted {
-                print("Autorisation pour les notifications accordée.")
+        var body: some Scene {
+            WindowGroup {
+                ContentView()
+                    .environmentObject(TodoManager.shared) // Utilisez l'instance singleton
             }
         }
-        UNUserNotificationCenter.current().delegate = self
-        return true
     }
-    
-    // Gérer ici la réception des notifications
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.banner, .badge, .sound])
-    }
-}
+        
+        // Gérer ici la réception des notifications
+        class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+            func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+                // Demande d'autorisation pour les notifications
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, _ in
+                    if granted {
+                        print("Autorisation pour les notifications accordée.")
+                    }
+                }
+                UNUserNotificationCenter.current().delegate = self
+                return true
+            }
+            
+            func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+                let title = notification.request.content.title
+
+                // Utilisez `TodoManager.shared` pour accéder au gestionnaire de todos
+                TodoManager.shared.addReceivedNotification(title: title, body: "La todo \(title) ne reste que 24h !")
+
+                completionHandler([.banner, .sound])
+            }
+        }
+
+
