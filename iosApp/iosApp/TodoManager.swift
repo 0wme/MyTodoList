3ef7041e863b9b9002c71db63ff866f1f59161f2
Tom Vieira <skixoxgaming@gmail.com>
2024-03-05 16:24:59 +0100

    Ajout de la notification dans TodoManager

--- iosApp/iosApp/TodoManager.swift
+++ iosApp/iosApp/TodoManager.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Combine
+import UserNotifications
 
 class TodoManager: ObservableObject {
     @Published var todosAFaire: [Todo] = []
@@ -8,6 +9,26 @@ class TodoManager: ObservableObject {
 
     func addTodo(_ todo: Todo) {
         todosAFaire.append(todo)
+        scheduleNotification(for: todo)
+    }
+    
+    private func scheduleNotification(for todo: Todo) {
+        guard let deadline = todo.date, deadline.timeIntervalSinceNow <= 86400 else { return }
+
+        let content = UNMutableNotificationContent()
+        content.title = "Rappel de Todo"
+        content.body = "Il reste moins de 24h pour accomplir '\(todo.title)'."
+        content.sound = UNNotificationSound.default
+
+        let timeInterval = max(deadline.timeIntervalSinceNow - 3600, 1) // Planifier pour 1 heure avant l'échéance, ou immédiatement si dans moins d'une heure.
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: false)
+
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Erreur lors de la planification de la notification: \(error.localizedDescription)")
+            }
+        }
     }
 
     func approve(todo: Todo) {
@@ -42,6 +63,8 @@ class TodoManager: ObservableObject {
             todosRetard.remove(at: index)
         }
     }
+    
+    
 }
 
 struct Todo: Identifiable {
