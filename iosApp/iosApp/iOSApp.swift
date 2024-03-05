00234b1f7b6e5601cc07f0c431680303ae874f10
Tom Vieira <skixoxgaming@gmail.com>
2024-03-05 16:23:51 +0100

    Création de la class AppDelegate pout autoriser les notifications

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,10 @@
 import SwiftUI
+import UserNotifications
 
 @main
 struct iOSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var todoManager = TodoManager()
 
     var body: some Scene {
@@ -9,5 +12,23 @@ struct iOSApp: App {
             ContentView()
                 .environmentObject(todoManager)
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // Demande d'autorisation pour les notifications
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, _ in
+            if granted {
+                print("Autorisation pour les notifications accordée.")
+            }
+        }
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+    
+    // Gérer ici la réception des notifications
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .badge, .sound])
     }
 }
