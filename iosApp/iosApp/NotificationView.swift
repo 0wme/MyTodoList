9066623aedaad5983744aefec6f04779aaf3e2fa
Tom Vieira <skixoxgaming@gmail.com>
2024-03-05 16:45:44 +0100

    Implémentation de NotificationView (1)

--- iosApp/iosApp/NotificationView.swift
+++ iosApp/iosApp/NotificationView.swift
@@ -1,7 +1,21 @@
 import SwiftUI
 
 struct NotificationView: View {
+    @EnvironmentObject var todoManager: TodoManager
+    
     var body: some View {
-        Text("Notification View")
+        VStack {
+            Text("Notifications").font(.largeTitle)
+            
+            List(todoManager.receivedNotifications) { notification in
+                VStack(alignment: .leading) {
+                    Text(notification.title).font(.headline)
+                    Text(notification.body).font(.subheadline)
+                }
+                .padding()
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(8)
+            }
+        }
     }
 }
