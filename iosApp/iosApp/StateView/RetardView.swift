a23af1797a07566aa8af346e952d93024185298c
Tom Vieira <skixoxgaming@gmail.com>
2024-03-08 16:40:53 +0100

    Uniformisation de RealiseView et RetardView ( final )

--- iosApp/iosApp/StateView/RetardView.swift
+++ iosApp/iosApp/StateView/RetardView.swift
@@ -5,13 +5,13 @@ struct RetardView: View {
 
     var body: some View {
         ScrollView {
-            LazyVStack(spacing: 10) {
+            LazyVStack(spacing: 20) {
                 ForEach(todoManager.todosRetard) { todo in
                     VStack(alignment: .leading) {
                         Text(todo.title).font(.headline)
                     }
                     .padding()
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .frame(maxWidth: .infinity, minHeight: 80, alignment: .leading)
                     .background(Color.gray.opacity(0.2))
                     .cornerRadius(8)
                     .contextMenu {
@@ -22,6 +22,7 @@ struct RetardView: View {
                 }
             }
             .padding(.horizontal)
+            .padding(.top, 25)
         }
         .navigationTitle("Retard")
     }
@@ -32,3 +33,4 @@ struct RetardView: View {
         }
     }
 }
+
