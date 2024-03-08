f5c67dc44c06ef82a6e8546579b396443072c43a
Tom Vieira <skixoxgaming@gmail.com>
2024-03-08 14:05:57 +0100

    Commencement de l'implémentation de SettingsView, ajout du text Paramètre

--- iosApp/iosApp/NaviguationView/SettingsView.swift
+++ iosApp/iosApp/NaviguationView/SettingsView.swift
@@ -1,7 +1,23 @@
 import SwiftUI
 
 struct SettingsView: View {
+    // Utilisé pour afficher une alerte de confirmation avant de réinitialiser la base de données
+    @State private var showingResetAlert = false
+    
     var body: some View {
-        Text("Settings View")
+        VStack {
+            Text("Paramètres").font(.largeTitle)
+                .frame(maxWidth: .infinity, alignment: .center)
+                .padding()
+            
+        }
+        
+    }
+    
+    
+    struct SettingsView_Previews: PreviewProvider {
+        static var previews: some View {
+            SettingsView()
+        }
     }
 }
