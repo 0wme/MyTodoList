da62a3dccb83d0f3a4a0128ba37b0bd63165afce
Tom Vieira <skixoxgaming@gmail.com>
2024-03-16 19:09:22 +0100

    Remise en état de NotificationView

--- iosApp/iosApp/NaviguationView/NotificationView.swift
+++ iosApp/iosApp/NaviguationView/NotificationView.swift
@@ -7,42 +7,22 @@ struct NotificationView: View {
         VStack {
             Text("Notifications").font(.largeTitle)
             
-            HStack {
-                Spacer()
-                Button("Clear all") {
-                    clearAllNotifications()
-                }
-                .padding(.bottom, 5)
-
-            }
-            
-            ScrollView {
-                LazyVStack(spacing: 10) {
-                    ForEach(todoManager.receivedNotifications) { notification in
-                        VStack(alignment: .leading) {
-                            Text(notification.title).font(.headline)
-                            Text(notification.body).font(.subheadline)
+            List(todoManager.receivedNotifications) { notification in
+                ScrollView {
+                    LazyVStack(spacing: 10) {
+                        ForEach(todoManager.receivedNotifications) { notification in
+                            VStack(alignment: .leading) {
+                                Text(notification.title).font(.headline)
+                                Text(notification.body).font(.subheadline)
+                            }
+                            .padding()
+                            .background(Color.gray.opacity(0.2))
+                            .cornerRadius(8)
                         }
-                        .padding()
-                        .background(Color.gray.opacity(0.2))
-                        .cornerRadius(8)
                     }
+                    .padding(.horizontal)
                 }
-                .padding(.horizontal)
             }
-        }
-        .padding(.horizontal)
-        .onAppear {
-            todoManager.loadNotificationsFromDB()
-        }
-    }
-    
-    private func clearAllNotifications() {
-        do {
-            try DatabaseManager.shared.deleteAllNotifications()
-            todoManager.receivedNotifications.removeAll()
-        } catch {
-            print("Error deleting all notifications: \(error)")
         }
     }
 }
